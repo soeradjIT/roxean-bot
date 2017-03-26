7faee3786299b8a999e5937aa2823e012db6b5c9
Robert Pemberton <ukrpe02@LYV.LiveNation.com>
2017-03-26 21:18:50 +0100

    udpate formatting

--- facts.js
+++ facts.js
@@ -1,18 +1,18 @@
 const facts = [
   {
-    fact: "Vending machines kill 4 times as many people as sharks per year"
+    fact: "Vending machines kill 4 times as many people as sharks per year."
   },
   {
-    fact: "Fredric Baur invented the Pringles can. When he passed away in 2008, his ashes were buried in one"
+    fact: "Fredric Baur invented the Pringles can. When he passed away in 2008, his ashes were buried in one."
   },
   {
-    fact: "Psychology is the brain trying to comprehend itself"
+    fact: "Psychology is the brain trying to comprehend itself."
   },
   {
-    fact: "The average four year-old child asks over four hundred questions a day"
+    fact: "The average four year-old child asks over four hundred questions a day."
   },
   {
-    fact: "A TI-83 calculator has six times more processing power than the computer that landed Apollo 11 on the moon"
+    fact: "A TI-83 calculator has six times more processing power than the computer that landed Apollo 11 on the moon."
   }
 ]
 
