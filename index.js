7f74df27bbcbf332614bd7cab120d9f43d1d15ec
Robert Pemberton <ukrpe02@LYV.LiveNation.com>
2017-03-26 17:44:39 +0100

    initial

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.set('port', (process.env.PORT || 3000))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.post('/', (req, res) => {
+  const text = req.body.text
+
+  const data = {
+    response_type: 'in_channel', // public to the channel
+    text: text
+  }
+
+  res.send(data)
+})
+
+const server = app.listen(app.get('port'), () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env)
+})
