9d8ac567bd930cb89868d4ece98d951ebb79d81a
Robert Pemberton <ukrpe02@LYV.LiveNation.com>
2017-03-31 17:28:50 +0100

    add attachments

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const facts = require('./facts')
 const request = require('request')
 const app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.set('port', (process.env.PORT || 3000))
+
 const getApiData = (req, res) => {
-    request.get('https://opentdb.com/api.php?amount=1&category=18', (err, response, body) => {
-        if (!err && response.statusCode == 200) {
-            res.send(body)
-        }
-    })
+  request.get('https://opentdb.com/api.php?amount=1&category=18', (err, response, body) => {
+    if (!err && response.statusCode == 200) {
+      res.send(JSON.parse(body))
+    }
+  })
 }
 
 const randomFact = () => {
@@ -18,36 +23,63 @@ const randomFact = () => {
   return facts[randomFactId]
 }
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.set('port', (process.env.PORT || 3000))
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 // response
 app.post('/', (req, res) => {
-  // const text = req.body.text
-  const getFact = randomFact();
-    let Fact = getFact.fact;
-    const data = {
-    response_type: 'in_channel',
-    text: Fact
 
   const text = req.body.text
+
   if(text === 'trivia') {
     getApiData(req, res)
   } else {  
-    const getFact = randomFact();
-      let Fact = getFact.fact;
-      const data = {
-      response_type: 'in_channel',
-      text: Fact
-    }
-    res.send(data)
+    const getFact = randomFact()
+    const fact = getFact.fact
 
+    const data = {
+      "response_type": "in_channel",
+      "text": fact,
+      "attachments": [
+          {
+              "text": "Choose a game to play",
+              "fallback": "You are unable to choose a game",
+              "callback_id": "wopr_game",
+              "color": "#3AA3E3",
+              "attachment_type": "default",
+              "actions": [
+                  {
+                      "name": "game",
+                      "text": "Chess",
+                      "type": "button",
+                      "value": "chess"
+                  },
+                  {
+                      "name": "game",
+                      "text": "Falken's Maze",
+                      "type": "button",
+                      "value": "maze"
+                  },
+                  {
+                      "name": "game",
+                      "text": "Thermonuclear War",
+                      "style": "danger",
+                      "type": "button",
+                      "value": "war",
+                      "confirm": {
+                          "title": "Are you sure?",
+                          "text": "Wouldn't you prefer a good game of chess?",
+                          "ok_text": "Yes",
+                          "dismiss_text": "No"
+                      }
+                  }
+              ]
+          }
+      ]
+    }
+
+    res.send(data)
   }
 })
 
